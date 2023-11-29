d4c5a7ec52fa569f843d067c9f738ee77af7e2ca
ragnamarie <ragna.steinboemer@gmail.com>
2023-11-29 14:10:26 +0100

    added dropdown

--- Components/AvailableSlots/index.js
+++ Components/AvailableSlots/index.js
@@ -1,4 +1,32 @@
-import React, { useState } from "react";
-import Dropdown from "./Dropdown";
+import Link from "next/link";
+import useSWR from "swr";
 
-export default function AvailableSlots() {}
+export default function AvailableSlots() {
+  const { data, isLoading } = useSWR("/api/availableSlots");
+
+  if (isLoading) {
+    return <h1>kick-off is just around the corner...</h1>;
+  }
+
+  if (!data) {
+    return;
+  }
+
+  console.log(data);
+
+  return (
+    <div>
+      <label>Select an available slot:</label>
+      <select>
+        {data.map((availableSlot) => (
+          <option
+            key={availableSlot.id}
+            value={`${availableSlot.time} - ${availableSlot.day} - ${availableSlot.location}`}
+          >
+            {availableSlot.time}, {availableSlot.day}, {availableSlot.location}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
--- Components/TeamDetails/index.js
+++ Components/TeamDetails/index.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import useSWR from "swr";
 import BookedSlots from "../BookedSlots";
+import AvailableSlots from "../AvailableSlots";
 
 export default function TeamDetails() {
   const router = useRouter();
@@ -10,7 +11,7 @@ export default function TeamDetails() {
   const { data, isLoading } = useSWR(`/api/teams/${id}`);
 
   if (isLoading) {
-    return <h1>Loading...</h1>;
+    return <h1>kick-off is just around the corner...</h1>;
   }
 
   if (!data) {
@@ -24,6 +25,7 @@ export default function TeamDetails() {
       <Link href="/club">← Back to List</Link>
       <h1>{data.name}</h1>
       <BookedSlots bookedTimeSlots={data.bookedTimeSlots} />
+      <AvailableSlots />
     </>
   );
 }
