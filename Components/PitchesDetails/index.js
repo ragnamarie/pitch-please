f271cf09bf1fc0de189cd18cf04e0d0af81f66a6
ragnamarie <ragna.steinboemer@gmail.com>
2023-12-02 13:13:59 +0100

    extracted info from object

--- Components/PitchesDetails/index.js
+++ Components/PitchesDetails/index.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { uid } from "uid";
 import Calendar from "react-calendar";
+import { useTable } from "react-table";
 
 export default function PitchesDetails({ availableTimeSlots }) {
   const uniquePitches = [];
@@ -31,15 +31,83 @@ export default function PitchesDetails({ availableTimeSlots }) {
     return <p>Pitch not found</p>;
   }
 
-  // Use the id directly
+  const filteredSlots = availableTimeSlots.filter(
+    (slot) => slot.location === name && !slot.isAvailable
+  );
+
+  const scheduleData = filteredSlots.map((slot) => ({
+    time: slot.time,
+    day: slot.day,
+    teamID: slot.teamID,
+  }));
+
+  console.log(filteredSlots);
+
+  // const scheduleData = [
+  //   {
+  //     time: "16:00 PM",
+  //     monday: "Team A",
+  //     tuesday: "Team B",
+  //     wednesday: "Team C",
+  //     thursday: "Team D",
+  //     friday: "Team E",
+  //   },
+  //   {
+  //     time: "17:30 PM",
+  //     monday: "Team F",
+  //     tuesday: "Team G",
+  //     wednesday: "Team H",
+  //     thursday: "Team I",
+  //     friday: "Team J",
+  //   },
+  //   {
+  //     time: "19:00 PM",
+  //     monday: "Team K",
+  //     tuesday: "Team L",
+  //     wednesday: "Team M",
+  //     thursday: "Team N",
+  //     friday: "Team O",
+  //   },
+  //   {
+  //     time: "20:30 PM",
+  //     monday: "Team P",
+  //     tuesday: "Team Q",
+  //     wednesday: "Team R",
+  //     thursday: "Team S",
+  //     friday: "Team T",
+  //   },
+  //   // Add more rows as needed
+  // ];
+
   return (
-    <>
+    <div>
       <Link href="/overview">← Back to List</Link>
-      <div>
-        <h1>{name}</h1>
-        {/* Render other details for the matched pitch if needed */}
-        <Calendar />
-      </div>
-    </>
+
+      <h1>{name}</h1>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednesday</th>
+            <th>Thursday</th>
+            <th>Friday</th>
+          </tr>
+        </thead>
+        <tbody>
+          {scheduleData.map((row, index) => (
+            <tr key={index}>
+              <td>{row.time}</td>
+              <td>{row.monday}</td>
+              <td>{row.tuesday}</td>
+              <td>{row.wednesday}</td>
+              <td>{row.thursday}</td>
+              <td>{row.friday}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
