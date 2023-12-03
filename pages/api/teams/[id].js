f7cf392a98f8d18d8b1d4f1c1c15f96e4acce13f
ragnamarie <ragna.steinboemer@gmail.com>
2023-12-03 15:56:01 +0100

    add delete button like a prooooo

--- pages/api/teams/[id].js
+++ pages/api/teams/[id].js
@@ -14,4 +14,10 @@ export default async function handler(request, response) {
 
     response.status(200).json(team);
   }
+
+  if (request.method === "DELETE") {
+    await Team.deleteOne({ slug: id });
+
+    response.status(200).json({ message: "Success!" });
+  }
 }
