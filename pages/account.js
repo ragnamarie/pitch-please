4be38a082d2a92cfacb4caf0f67aa191611ab812
ragnamarie <ragna.steinboemer@gmail.com>
2023-12-05 13:46:39 +0100

    login check

--- pages/account.js
+++ pages/account.js
@@ -1,12 +1,33 @@
-import { useSession, signOut } from "next-auth/react";
+import { useSession, signOut, getSession } from "next-auth/react";
 
 const Account = () => {
-  const { data: session } = useSession();
-  return (
-    <div>
-      <p>You are not signed in</p>
-    </div>
-  );
+  const { data: session, status } = useSession();
+
+  if (status === "authenticated") {
+    return (
+      <div>
+        <p>Welcome {session.user.name}</p>
+        <button onClick={() => signOut()}>SIGN OUT</button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <p>You are not signed in</p>
+      </div>
+    );
+  }
 };
 
+export const getServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: { destination: "/login" },
+    };
+  }
+
+  return { props: { session } };
+};
 export default Account;
