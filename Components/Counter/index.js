52891f3195d2f2d38b985261dc1c23a65a7af0a2
ragnamarie <ragna.steinboemer@gmail.com>
2023-12-07 14:59:28 +0100

    counter fixed

--- Components/Counter/index.js
+++ Components/Counter/index.js
@@ -1,6 +1,19 @@
+import { useSession } from "next-auth/react";
+import useSWR from "swr";
+
 export default function Counter({ availableTimeSlots }) {
+  const { data: session, status } = useSession();
+
+  const { data, isLoading, error } = useSWR(
+    session ? `api/users/${session.user?.googleId}` : null
+  );
+
+  const userClub = data?.[0]?.clubName; // Ensure data is available before accessing its properties
+
   const availableSlotsFilter = availableTimeSlots.filter(
-    (availableTimeSlot) => availableTimeSlot.isAvailable === true
+    (availableTimeSlot) =>
+      availableTimeSlot.isAvailable === true &&
+      availableTimeSlot.clubName === userClub
   );
   const availableSlotsCount = availableSlotsFilter.length;
 
